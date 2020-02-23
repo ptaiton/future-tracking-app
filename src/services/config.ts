51c30638305b97eb9684a8bc3fb654953a5c4227
Pierre <pierre.taiton@outlook.fr>
2020-02-23 19:58:24 +0100

    update

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,5 +1,5 @@
 
 export const config = {
   SUMOLOGIC_ENDPOINT_URL: process.env.REACT_APP_SUMOLOGIC_ENDPOINT_URL,
-  API_URL: process.env.API_URL
+  API_URL: process.env.API_URL || 'http://localhost:8080'
 }
